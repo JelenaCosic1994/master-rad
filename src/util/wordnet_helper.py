f5ccba7b44baacbb74612fcc239121d76609bb3e
JelenaCosic1994 <jelenacosic94@gmail.com>
2019-12-15 20:25:43 +0100

    make local variables and renaming

--- src/util/wordnet_helper.py
+++ src/util/wordnet_helper.py
@@ -5,20 +5,21 @@ import xml.etree.ElementTree as et
 class WordNetHelper:
 
     def __init__(self, english_wordnet, serbian_wordnet_path):
-        self._map_id_and_pos_neg_score = {}  # id : list(pos, neg score)  much bigger than _map_serbian_wordnet
-        self._map_serbian_wordnet = {}  # id : tuple(words)
+        map_serbian_wordnet = self._create_map_serbian_wordnet(serbian_wordnet_path)
+        map_id_and_pos_neg_score = self._create_map_id_and_pos_neg_score(english_wordnet['ID'], english_wordnet['PosScore'], english_wordnet['NegScore'])
+        self._wordnet_data = self._create_wordnet_data(map_serbian_wordnet, map_id_and_pos_neg_score)
 
-        self._create_map_serbian_wordnet(serbian_wordnet_path)
-        self._create_map_id_and_pos_neg_score(english_wordnet['ID'], english_wordnet['PosScore'], english_wordnet['NegScore'])
-
-        self._data_frame_wordnet = self._create_data_frame()
-
-    def _create_map_id_and_pos_neg_score(self, id_in_wnen, pos_scores_in_wnen, neg_scores_in_wnen):
+    @staticmethod
+    def _create_map_id_and_pos_neg_score(id_in_wnen, pos_scores_in_wnen, neg_scores_in_wnen):
+        map_id_and_pos_neg_score = {}
         for i in range(len(id_in_wnen)):
             br_str = converter.convert_from_float_to_string(id_in_wnen[i])
-            self._map_id_and_pos_neg_score[br_str] = [pos_scores_in_wnen[i], neg_scores_in_wnen[i]]  # TODO use tuple
+            map_id_and_pos_neg_score[br_str] = [pos_scores_in_wnen[i], neg_scores_in_wnen[i]]  # TODO use tuple
+        return map_id_and_pos_neg_score
 
-    def _create_map_serbian_wordnet(self, xml_doc_path):
+    @staticmethod
+    def _create_map_serbian_wordnet(xml_doc_path):
+        map_serbian_wordnet = {}
         xml_doc = et.parse(xml_doc_path)
         root = xml_doc.getroot()
         for synset in root.findall('SYNSET'):
@@ -28,18 +29,20 @@ class WordNetHelper:
                 list_literal = []
                 for literal in synonym.findall('LITERAL'):
                     list_literal.append(converter.convert_serbian_word_to_aurora(literal.text))
-                self._map_serbian_wordnet[id_] = tuple(list_literal)
+                map_serbian_wordnet[id_] = tuple(list_literal)
+        return map_serbian_wordnet
 
-    def _create_data_frame(self):
+    @staticmethod
+    def _create_wordnet_data(map_serbian_wordnet, map_id_and_pos_neg_score):
         ids = []
         scores = []
         literals = []
 
-        for id_ in self._map_serbian_wordnet:
-            if id_ in self._map_id_and_pos_neg_score:
+        for id_ in map_serbian_wordnet:
+            if id_ in map_id_and_pos_neg_score:
                 ids.append(id_)
-                scores.append(self._map_id_and_pos_neg_score[id_])
-                literals.append(self._map_serbian_wordnet[id_])
+                scores.append(map_id_and_pos_neg_score[id_])
+                literals.append(map_serbian_wordnet[id_])
 
         data = {"id": ids,
                 "score": scores,
@@ -48,18 +51,18 @@ class WordNetHelper:
         return data
 
     def get_pos_neg_score_for_serbian_word(self, word):
-
         pos_scores = []
         neg_scores = []
 
-        for i in range(len(self._data_frame_wordnet["id"])):
-            literals = self._data_frame_wordnet["literals"][i]
+        for i in range(len(self._wordnet_data["id"])):
+            literals = self._wordnet_data["literals"][i]
             for literal in literals:
                 if word in literal:
-                    score = self._data_frame_wordnet["score"][i]
-                    pos_scores.append(score[0])
-                    neg_scores.append(score[1])
-                    break
+                    score = self._wordnet_data["score"][i]
+                    if score[0] != score[1]:
+                        pos_scores.append(score[0])
+                        neg_scores.append(score[1])
+                        break
         if len(pos_scores) > 0:
             return sum(pos_scores) / len(pos_scores), sum(neg_scores) / len(neg_scores)
         else:
